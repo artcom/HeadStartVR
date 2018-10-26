135ffb37397a261d036fa9bdcd4df143ae6a932a
Khaled <khaled.reguieg@artcom.de>
2018-10-26 12:36:05 +0200

    :rocket: Initial commit of HeadStartVR :rocket:

---
+++ Assets/VRTK/Examples/ExampleResources/SceneResources/[005 - Interactions] InteractableObjects/Scripts/ControllableReactor.cs
@@ -0,0 +1,61 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using UnityEngine.UI;
+    using VRTK.Controllables;
+
+    public class ControllableReactor : MonoBehaviour
+    {
+        public VRTK_BaseControllable controllable;
+        public Text displayText;
+        public string outputOnMax = "Maximum Reached";
+        public string outputOnMin = "Minimum Reached";
+
+        public UnityEvent m_SetTimeline;
+
+        float timer;
+        private bool timeSwitch;
+
+        private void Update() {
+            timer+= Time.deltaTime;
+            if(timer >= 3.0f)
+                timeSwitch = true;
+        }
+
+        protected virtual void OnEnable()
+        {
+            controllable = (controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable);
+            controllable.ValueChanged += ValueChanged;
+            controllable.MaxLimitReached += MaxLimitReached;
+            controllable.MinLimitReached += MinLimitReached;
+        }
+
+        protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+        {
+            if (displayText != null)
+            {
+                displayText.text = e.value.ToString("F1");
+            }
+        }
+
+        protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
+        {
+            if(timeSwitch){
+                m_SetTimeline.Invoke();
+                if (outputOnMax != "")
+                {
+                    Debug.Log(outputOnMax);
+                }
+            }
+        }
+
+        protected virtual void MinLimitReached(object sender, ControllableEventArgs e)
+        {
+            if (outputOnMin != "")
+            {
+                Debug.Log(outputOnMin);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # HeadStartVR
-This repository holds VRTK, CustomHands with forearm, overriding avatarmasks which provide real finger movement depening on which button was pressed, basic grabbing of objects highlighting on touch and near touch and bezier-pointer teleporter where you can toggle between dash and blink teleporter.
+This repository holds everything you need to headstart your own VR project:
+* VRTK
+* CustomHands with forearm
+* overriding avatarmasks which provide real finger movement depening on which button was pressed
+* basic grabbing of objects
+* highlighting on touch and near touch
+* bezier-pointer teleporter
+** where you can toggle between dash and blink teleporter.
+
+## Software
+* UnityHub
+* Unity 2018.2.6f1
+* VRTK 3.2.1
+* SteamVR Plugin
+
+### Unity Project Organisation
+
+Please keep the project clean and tidy, so noone gets confused to much!
+The project should be organised as follows:
+* Assets
+* MainApplication
+* Animations
+* Models
+* Materials
+* Scripts
+* Scenes
+* Feature1
+* Animations
+* Models
+* Materials
+* Scripts
+* Scenes
+* Feature2
+* Animations
+* Models
+* Materials
+* Scripts
+* Scenes
+* ProjectSettings
+* UnityPackages
+
+Read and follow those rules [Unity Project Organisation](https://unity3d.com/de/learn/tutorials/topics/tips/large-project-organisation).
+
+### Code Styleguide
+
+We try to follow this [Unity C# code styleguide](https://github.com/raywenderlich/c-sharp-style-guide).
+Feel free to discuss those guidelines.
