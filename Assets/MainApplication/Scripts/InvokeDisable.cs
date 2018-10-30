5e5e8cda77ef7eb58a0bac7ca928a796c2725c3e
Khaled <khaled.reguieg@artcom.de>
2018-10-30 18:24:59 +0100

    [ADD] Disabling SteamVR Controllers on Start. Correct rotation of hand model.

---
+++ Assets/MainApplication/Scripts/InvokeDisable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class InvokeDisable : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+		Invoke("DisableAfterStart", 1.5f);
+	}
+
+	void DisableAfterStart()
+	{
+		gameObject.SetActive(false);
+	}
+}
